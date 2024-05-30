4928ab1b8fd43a2235ebde79b340d9b7c15fe767
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-05-30 22:31:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('')
+            k = await query.message.edit('Not found in my database...')
             await asyncio.sleep(10)
             await k.delete()
 
