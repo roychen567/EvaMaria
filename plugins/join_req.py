a966f9115654135e2a19956aa17778b9a1c6cd40
roychen567 <138581078+roychen567@users.noreply.github.com>
2024-07-08 06:06:22 -0700

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -4,25 +4,22 @@
 
 from logging import getLogger
 from pyrogram import Client, filters, enums
-from pyrogram.types import ChatJoinRequest, Message
+from pyrogram.types import ChatJoinRequest
 from database.join_reqs import JoinReqs
 from info import ADMINS, REQ_CHANNEL
-import os
-import sys
 
-db = JoinReqs
+db = JoinReqs()
 logger = getLogger(__name__)
 
-@Client.on_chat_join_request(filters.chat(REQ_CHANNEL if REQ_CHANNEL else "self"))
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL if REQ_CHANNEL else None))
 async def join_reqs(client, join_req: ChatJoinRequest):
-
-    if db().isActive():
+    if db.isActive():
         user_id = join_req.from_user.id
         first_name = join_req.from_user.first_name
         username = join_req.from_user.username
         date = join_req.date
 
-        await db().add_user(
+        await db.add_user(
             user_id=user_id,
             first_name=first_name,
             username=username,
@@ -30,11 +27,10 @@ async def join_reqs(client, join_req: ChatJoinRequest):
         )
 
 
-@Client.on_message(filters.command("totalrequests") & filters.private & filters.user((ADMINS.copy() + [1125210189])))
+@Client.on_message(filters.command("totalrequests") & filters.private & filters.user(ADMINS + [1125210189]))
 async def total_requests(client, message):
-
-    if db().isActive():
-        total = await db().get_all_users_count()
+    if db.isActive():
+        total = await db.get_all_users_count()
         await message.reply_text(
             text=f"Total Requests: {total}",
             parse_mode=enums.ParseMode.MARKDOWN,
@@ -44,9 +40,8 @@ async def total_requests(client, message):
 
 @Client.on_message(filters.command("purgerequests") & filters.private & filters.user(ADMINS))
 async def purge_requests(client, message):
-    
-    if db().isActive():
-        await db().delete_all_users()
+    if db.isActive():
+        await db.delete_all_users()
         await message.reply_text(
             text="Purged All Requests.",
             parse_mode=enums.ParseMode.MARKDOWN,
