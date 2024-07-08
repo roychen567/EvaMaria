efdbb0e48604a7d19e04624938a141a30b389f50
roychen567 <138581078+roychen567@users.noreply.github.com>
2024-07-08 05:32:07 -0700

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -4,14 +4,18 @@ from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
 
-
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    for file_type in ("document", "video", "audio"):
-        media = getattr(message, file_type, None)
-        if media is not None:
-            break
+    if message.document:
+        media = message.document
+        file_type = "document"
+    elif message.video:
+        media = message.video
+        file_type = "video"
+    elif message.audio:
+        media = message.audio
+        file_type = "audio"
     else:
         return
 
