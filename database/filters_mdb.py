700dbc70a69fb6885429a79fac149886e684fc14
roychen567 <138581078+roychen567@users.noreply.github.com>
2024-07-08 05:05:18 -0700

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -2,80 +2,79 @@ import pymongo
 from pyrogram import enums
 from info import DATABASE_URI, DATABASE_NAME
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
-
-
-async def add_filter(grp_id, text, reply_text, btn, file, alert):
+def add_filter(grp_id, text, reply_text, btn, file, alert):
     mycol = mydb[str(grp_id)]
     # mycol.create_index([('text', 'text')])
 
     data = {
-        'text':str(text),
-        'reply':str(reply_text),
-        'btn':str(btn),
-        'file':str(file),
-        'alert':str(alert)
+        'text': str(text),
+        'reply': str(reply_text),
+        'btn': str(btn),
+        'file': str(file),
+        'alert': str(alert)
     }
 
     try:
-        mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
-    except:
-        logger.exception('Some error occured!', exc_info=True)
-             
-     
-async def find_filter(group_id, name):
+        mycol.update_one({'text': str(text)}, {"$set": data}, upsert=True)
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+        return False
+    return True
+
+def find_filter(group_id, name):
     mycol = mydb[str(group_id)]
-    
-    query = mycol.find( {"text":name})
-    # query = mycol.find( { "$text": {"$search": name}})
+
     try:
+        query = mycol.find({"text": name})
         for file in query:
             reply_text = file['reply']
             btn = file['btn']
             fileid = file['file']
-            try:
-                alert = file['alert']
-            except:
-                alert = None
-        return reply_text, btn, alert, fileid
-    except:
+            alert = file.get('alert', None)
+            return reply_text, btn, alert, fileid
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
         return None, None, None, None
 
-
-async def get_filters(group_id):
+def get_filters(group_id):
     mycol = mydb[str(group_id)]
-
     texts = []
-    query = mycol.find()
+
     try:
+        query = mycol.find()
         for file in query:
             text = file['text']
             texts.append(text)
-    except:
-        pass
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+        return []
     return texts
-
 
 async def delete_filter(message, text, group_id):
     mycol = mydb[str(group_id)]
-    
-    myquery = {'text':text }
-    query = mycol.count_documents(myquery)
-    if query == 1:
-        mycol.delete_one(myquery)
-        await message.reply_text(
-            f"'`{text}`'  deleted. I'll not respond to that filter anymore.",
-            quote=True,
-            parse_mode=enums.ParseMode.MARKDOWN
-        )
-    else:
-        await message.reply_text("Couldn't find that filter!", quote=True)
 
+    myquery = {'text': text}
+    try:
+        query = mycol.count_documents(myquery)
+        if query == 1:
+            mycol.delete_one(myquery)
+            await message.reply_text(
+                f"'`{text}`' deleted. I'll not respond to that filter anymore.",
+                quote=True,
+                parse_mode=enums.ParseMode.MARKDOWN
+            )
+        else:
+            await message.reply_text("Couldn't find that filter!", quote=True)
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+        await message.reply_text("An error occurred while trying to delete the filter.", quote=True)
 
 async def del_all(message, group_id, title):
     if str(group_id) not in mydb.list_collection_names():
@@ -85,30 +84,36 @@ async def del_all(message, group_id, title):
     mycol = mydb[str(group_id)]
     try:
         mycol.drop()
-        await message.edit_text(f"All filters from {title} has been removed")
-    except:
-        await message.edit_text("Couldn't remove all filters from group!")
-        return
+        await message.edit_text(f"All filters from {title} have been removed")
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+        await message.edit_text("Couldn't remove all filters from the group!")
 
-
-async def count_filters(group_id):
+def count_filters(group_id):
     mycol = mydb[str(group_id)]
 
-    count = mycol.count()
-    return False if count == 0 else count
+    try:
+        count = mycol.count_documents({})
+        return False if count == 0 else count
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+        return False
 
-
-async def filter_stats():
+def filter_stats():
     collections = mydb.list_collection_names()
 
     if "CONNECTION" in collections:
         collections.remove("CONNECTION")
 
     totalcount = 0
-    for collection in collections:
-        mycol = mydb[collection]
-        count = mycol.count()
-        totalcount += count
+    try:
+        for collection in collections:
+            mycol = mydb[collection]
+            count = mycol.count_documents({})
+            totalcount += count
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+        return 0, 0
 
     totalcollections = len(collections)
 
