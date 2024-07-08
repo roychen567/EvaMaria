f043bbc3dd177ecae86bb3dc0fa400949fc79ed7
roychen567 <138581078+roychen567@users.noreply.github.com>
2024-07-08 05:22:30 -0700

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,9 +4,14 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("MATRIX")
-  
+    return web.json_response({"message": "MATRIX"})
+
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, host="0.0.0.0", port=8080)
