11ee4a91d8858aab943a5a194ce8ccead1485ff3
roychen567 <138581078+roychen567@users.noreply.github.com>
2025-02-15 23:17:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,20 +15,20 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21958444'))
 API_HASH = environ.get('API_HASH', 'c7c23aa495b8cfa4cd784efab9de559f')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7308180365:AAGIP7mKBt8CXoGEVMap5Hb35BD01vnM7OI')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8108479997:AAH8YQ3EGPBQBnNaBQlyKKSt3dOpbstbFr4')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', '300'))
 USE_CAPTION_FILTER = is_enabled(environ.get('USE_CAPTION_FILTER', 'False'), False)
-PICS = environ.get('PICS', 'https://graph.org/file/9c023dd906352100948c8.mp4')
+PICS = environ.get('PICS', 'https://graph.org/file/9837e20755a4e559e0bd6-26ca2b16d0f1a1b1d6.jpg')
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7246677025').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002176523928').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7187011796').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002203759750').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '0'))  # Assuming AUTH_CHANNEL is an integer
-AUTH_GROUPS = [int(ch) for ch in environ.get('AUTH_GROUP', '').split()] if environ.get('AUTH_GROUP') else []
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001868251572'))  # Assuming AUTH_CHANNEL is an integer
+AUTH_GROUPS = [int(ch) for ch in environ.get('AUTH_GROUP', '-1001697068765').split()] if environ.get('AUTH_GROUP') else []
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mark42:mark42@mark.rib7tvo.mongodb.net/?retryWrites=true&w=majority&appName=mark")
@@ -36,12 +36,12 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "mark")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Request Fsub
-REQ_CHANNEL = int(environ.get("REQ_CHANNEL", "-1002188987730")) if environ.get("REQ_CHANNEL") else None
+REQ_CHANNEL = int(environ.get("REQ_CHANNEL", "-1002486327315")) if environ.get("REQ_CHANNEL") else None
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002161453442'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MOVIES_ZILAA')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002438691201'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MOVIE_BAZAR')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', 'True'), False)
 IMDB = is_enabled(environ.get('IMDB', 'False'), True)
 SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', 'True'), False)
