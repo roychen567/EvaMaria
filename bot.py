7478c1e9cc49b792386b76fde1c161622014940a
roychen567 <138581078+roychen567@users.noreply.github.com>
2025-02-16 00:33:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ class Bot(Client):
             plugins=dict(root="plugins")  # Ensure plugins are loaded
         )
 
- async def start(self, **kwargs):
+ async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
